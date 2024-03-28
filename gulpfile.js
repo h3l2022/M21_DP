f52e42c3b2f8e1daf5f23c55806091c843159368
Lucas Mario <lucasmariocontato@gmail.com>
2024-03-28 14:56:37 -0300

    segundo

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemim = require('gulp-imagemin');
 
-function styles(){
+function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyles: 'compressed' }))
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+   return gulp.src('./src/images/**/*')
+    .pipe(imagemim())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
